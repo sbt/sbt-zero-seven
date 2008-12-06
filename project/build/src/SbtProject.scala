9a547e4c95a93697b0ccec579f6c80053648601c
Mark Harrah <dmharrah@gmail.com>
2008-12-06 21:50:43 +0000

    Building sbt with sbt part 2.

--- project/build/src/SbtProject.scala
+++ project/build/src/SbtProject.scala
@@ -3,7 +3,7 @@ import sbt._
 class SbtProject(info: ProjectInfo) extends DefaultProject(info)
 {
 	override def defaultJarBaseName = "sbt-" + version.toString
-	
-	
-	println("Main classes: " + ((mainCompilePath) *** "*.class").get)
+	def extraResources = descendents(info.projectPath / "licenses", "*") +++ "LICENSE" +++ "NOTICE"
+	override def mainResources = super.mainResources +++ extraResources
+	override def mainClass = Some("sbt.Main")
 }
