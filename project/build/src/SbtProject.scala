9c245ab5d004a68b1b3c774afd0704a856e7082c
Mark Harrah <dmharrah@gmail.com>
2009-01-24 19:25:04 +0000

    A couple comments and a small change to sbt's project definition.

--- project/build/src/SbtProject.scala
+++ project/build/src/SbtProject.scala
@@ -3,8 +3,10 @@ import sbt._
 class SbtProject(info: ProjectInfo) extends DefaultProject(info)
 {
 	override def defaultJarBaseName = "sbt-" + version.toString
+	/** Additional resources to include in the produced jar.*/
 	def extraResources = descendents(info.projectPath / "licenses", "*") +++ "LICENSE" +++ "NOTICE"
 	override def mainResources = super.mainResources +++ extraResources
+	/** This specifies the Main-Class attribute in the manifest.*/
 	override def mainClass = Some("sbt.Main")
-	override def testOptions = ExcludeTests("sbt.ReflectiveSpecification" :: Nil) :: Nil
+	override def testOptions = ExcludeTests("sbt.ReflectiveSpecification" :: Nil) :: super.testOptions.toList
 }
