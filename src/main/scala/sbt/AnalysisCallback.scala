d01f89aedb869f4e00a2b8ff8edd26302556b0cb
Mark Harrah <dmharrah@gmail.com>
2009-01-26 02:17:03 +0000

    Added some documentation to AnalysisCallback

--- src/main/scala/sbt/AnalysisCallback.scala
+++ src/main/scala/sbt/AnalysisCallback.scala
@@ -1,5 +1,5 @@
 /* sbt -- Simple Build Tool
- * Copyright 2008 Mark Harrah
+ * Copyright 2008, 2009  Mark Harrah
  */
 package sbt
 
@@ -25,17 +25,30 @@ object AnalysisCallback
 
 trait AnalysisCallback extends NotNull
 {
+	/** The names of classes that the analyzer should find subclasses of.*/
 	def superclassNames: Iterable[String]
+	/** The base path for the project.*/
 	def basePath: Path
-	
+	/** Called when the the given superclass could not be found on the classpath by the compiler.*/
 	def superclassNotFound(superclassName: String): Unit
-	
+	/** Called before the source at the given location is processed. */
 	def beginSource(sourcePath: Path): Unit
+	/** Called when the a subclass of one of the classes given in <code>superclassNames</code> is
+	* discovered.*/
 	def foundSubclass(sourcePath: Path, subclassName: String, superclassName: String, isModule: Boolean): Unit
+	/** Called to indicate that the source file <code>sourcePath</code> depends on the source file
+	* <code>dependsOnPath</code>.*/
 	def sourceDependency(dependsOnPath: Path, sourcePath: Path): Unit
+	/** Called to indicate that the source file <code>sourcePath</code> depends on the jar
+	* <code>jarPath</code>.*/
 	def jarDependency(jarPath: File, sourcePath: Path): Unit
+	/** Called to indicate that the source file <code>sourcePath</code> depends on the class file
+	* <code>classFile</code>.*/
 	def classDependency(classFile: File, sourcePath: Path): Unit
+	/** Called to indicate that the source file <code>sourcePath</code> produces a class file at
+	* <code>modulePath</code>.*/
 	def generatedClass(sourcePath: Path, modulePath: Path): Unit
+	/** Called after the source at the given location has been processed. */
 	def endSource(sourcePath: Path): Unit
 }
 abstract class BasicAnalysisCallback(val basePath: Path, val superclassNames: Iterable[String],
